768f9c3819826229d658a970cb22e55c19e3c786
xiangzuhua <xiangzhuhua0209@126.com>
2021-06-03 17:42:14 +0800

    Initial Commit

---
+++ XHPageView/XHPageView.h
@@ -0,0 +1,18 @@
+//
+//  XHPageView.h
+//  XHPageView
+//
+//  Created by xiangzuhua on 2021/6/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XHPageView.
+FOUNDATION_EXPORT double XHPageViewVersionNumber;
+
+//! Project version string for XHPageView.
+FOUNDATION_EXPORT const unsigned char XHPageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XHPageView/PublicHeader.h>
+
+
